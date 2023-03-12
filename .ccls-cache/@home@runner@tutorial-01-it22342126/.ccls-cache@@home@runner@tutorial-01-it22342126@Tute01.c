8607f10b2a499d812accc3b0ec146ab4265f1eff
it22342126 <127610822+it22342126@users.noreply.github.com>
2023-03-12 08:01:51 +0000

    main function

--- .ccls-cache/@home@runner@tutorial-01-it22342126/.ccls-cache@@home@runner@tutorial-01-it22342126@Tute01.c
+++ .ccls-cache/@home@runner@tutorial-01-it22342126/.ccls-cache@@home@runner@tutorial-01-it22342126@Tute01.c
@@ -5,21 +5,18 @@
 #include <stdio.h>
 
 int main() {
-  //declaring variables
   int num1, num2;
   float avg;
 
-  //taking inputs
   printf("Enter first number : ");
   scanf("%d", &num1);
-  
   printf("Enter second number : ");
   scanf("%d", &num2);
 
   //calculations
   avg = (num1 + num2) / 2.0;
 
-  //printing the average of the two marks. %.2f is used for displaying output upto two decimal places
+  //%.2f is used for displaying output upto two decimal places
   printf("Average of %d and %d is : %.2f", num1,num2,avg);
   
   return 0;
